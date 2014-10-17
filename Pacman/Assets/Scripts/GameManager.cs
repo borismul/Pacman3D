e922f5a53ec69b181b19b734dc0d430c34804dd6
Tjitte <tp.dejong@mail.com>
2014-10-17 22:02:49 +0200

    Adding enemy AI

--- Pacman/Assets/Scripts/GameManager.cs
+++ Pacman/Assets/Scripts/GameManager.cs
@@ -6,12 +6,10 @@ public class GameManager : MonoBehaviour
     // Prefabs
     public Maze mazePrefab;
     public CoinSpawner coinSpawnerPrefab;
-    public MarkerSpawner pathMarkerSpawnerPrefab;
 
     // Instances
     private Maze maze;
     private CoinSpawner coinSpawner;
-    private MarkerSpawner pathMarkerSpawner;
 
 	// Use this for initialization
 	void Start () 
@@ -37,10 +35,5 @@ public class GameManager : MonoBehaviour
         coinSpawner = Instantiate(coinSpawnerPrefab) as CoinSpawner;
         coinSpawner.name = "Spawned coins";
         coinSpawner.Spawn(maze);
-
-        // Initialize the path marker spawner
-        pathMarkerSpawner = Instantiate(pathMarkerSpawnerPrefab) as MarkerSpawner;
-        pathMarkerSpawner.name = "Spawned path markers";
-        pathMarkerSpawner.Spawn(maze);
     }
 }
